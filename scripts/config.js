57cba39a69b0b7e392d2765c1a94b05ffda85c4d
verbascun <verbascun@protonmail.com>
2025-10-04 18:30:15 +0200

    0.1.1 comfig.js added so hit pages work corectly

---
+++ scripts/config.js
@@ -0,0 +1,11 @@
+// config.js
+var DASHBOARD_CONFIG = {
+  timezone: 'Europe/Warsaw',        // just a label for reference
+  standardOffset: 1,                // CET UTC+1 offset during standard time (CET)
+  dstOffset: 2,                     // CEST UTC+2 offset during daylight saving (CEST)
+  latitude: 51.300954064150204,     // Wieruszów
+  longitude: 18.16495781270237      // Wieruszów
+};
+
+
+
